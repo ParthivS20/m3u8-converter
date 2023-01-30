b44ae532ebeb639a9e593cec23e08eb8acbbb9b3
Parthiv Shah <42523433+ParthivS20@users.noreply.github.com>
2023-01-29 20:46:37 -0600

    Delete server/build directory

--- server/build/server/types/MediaType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-exports.MediaType = void 0;
-var MediaType;
-(function (MediaType) {
-    MediaType[MediaType["MOVIE"] = 0] = "MOVIE";
-    MediaType[MediaType["TV_SHOW"] = 1] = "TV_SHOW";
-})(MediaType = exports.MediaType || (exports.MediaType = {}));
