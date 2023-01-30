9c1a30ef1172a1e09bb40abf24924b83b92fc454
Parthiv Shah <42523433+ParthivS20@users.noreply.github.com>
2023-01-29 20:43:10 -0600

    Delete server/dist directory

--- server/dist/server/types/Directory.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-exports.Directory = void 0;
-var Directory;
-(function (Directory) {
-    Directory["TV_SHOWS"] = "TV SHOWS";
-    Directory["MOVIES"] = "Movies";
-})(Directory = exports.Directory || (exports.Directory = {}));
--- server/dist/server/types/Media.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {value: true});
-exports.Media = void 0;
-var Media;
-(function (Media) {
-    Media[Media["MOVIE"] = 0] = "MOVIE";
-    Media[Media["TV_SHOW"] = 1] = "TV_SHOW";
-})(Media = exports.Media || (exports.Media = {}));
